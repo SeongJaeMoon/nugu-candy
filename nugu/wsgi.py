193adeac53317770c79dd6c095aa3e0216901002
SeongjaeMoon <seongjae.m@gmail.com>
2018-11-16 21:47:14 +0900

    temp

--- nugu/wsgi.py
+++ nugu/wsgi.py
@@ -12,10 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nugu.settings')
+
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
-
-import django
-django.setup()
