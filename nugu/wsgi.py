1e379ce47721b4069274730469284aba65fdb029
SeongjaeMoon <seongjae.m@gmail.com>
2018-11-16 20:53:04 +0900

    init commit2

--- nugu/wsgi.py
+++ nugu/wsgi.py
@@ -12,5 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nugu.settings')
-
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
