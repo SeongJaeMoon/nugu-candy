9d415b648361b8bd1e73c5cfcdafe70fcb97d02a
SeongjaeMoon <seongjae.m@gmail.com>
2018-11-20 14:56:39 +0900

    fixed url path

--- nugu_data/urls.py
+++ nugu_data/urls.py
@@ -3,7 +3,7 @@ from nugu_data import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^health/$', views.health, name='health'),
-    url(r'^awCalorie/$', views.awCalorie, name='awCalorie'),
-    url(r'^awBmi/$', views.awBmi, name="awBmi"),
+    url(r'^health$', views.health, name='health'),
+    url(r'^awCalorie$', views.awCalorie, name='awCalorie'),
+    url(r'^awBmi$', views.awBmi, name="awBmi"),
 ]
